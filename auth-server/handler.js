e5cb59fde337d67d8637b306bcbfec0d75feddb3
Caduella <146942933+Caduella@users.noreply.github.com>
2024-03-21 08:40:30 +0100

    changed the url name

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -4,7 +4,7 @@ const { google } = require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = ["https://Caduella.github.io/meet/"];
+const redirect_uris = ["https://caduella.github.io/Meet/"];
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
