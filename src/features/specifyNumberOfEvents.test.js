cd1aea88e32bd9cde100bb01046808d62a694c7b
Caduella <146942933+Caduella@users.noreply.github.com>
2024-03-30 17:31:12 +0100

    EndToEnd test added

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -7,32 +7,30 @@ const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 defineFeature(feature, test => {
  // SCENARIO 1
-  test('When user hasn’t specified a number, 32 is the default number', ({ given, when, then }) => {
-    let AppComponent;
-    let eventList;
-    given('the user has not specified or filtered any number,', () => {
-      AppComponent = render(<App />);
+ test('When user hasn’t specified a number, 32 is the default number.', ({ given, when, then }) => {
+  let AppComponent;
+  let eventList;  
+  given('the user has not specified or filtered any number', () => {
+    AppComponent = render(<App />);
+  });
 
+  when('the user sees the list', async () => {
+    const AppDOM = AppComponent.container.firstChild;
+    await waitFor(() => {
+      eventList = within(AppDOM).queryAllByRole('listitem');
+      expect(eventList[0]).toBeTruthy();
     });
+  });
 
-    when('the user sees the list,', async () => {
-      const AppDOM = AppComponent.container.firstChild;
-      await waitFor(() => {
-        eventList = within(AppDOM).queryAllByRole('listitem');
-        expect(eventList[0]).toBeTruthy();
-      });
-    });
-
-    then(/^the default number of displayed events should be (\d+)$/, (arg0) => {
-      expect(eventList.length).toEqual(32);
-
-    });
+  then(/^the default number of displayed events should be (\d+)$/, (arg0) => {
+    expect(eventList.length).toEqual(32);
+  });
 });
 
  // SCENARIO 2
-test('User can change the number of events they want to see', ({ given, when, then }) => {
+ test('User can change the number of events they want to see.', ({ given, when, then }) => {     
   let AppComponent;
-  given('the user has events displayed,', async () => {
+  given('the user has events displayed', async () => {
     AppComponent = render(<App />);
     const AppDOM = AppComponent.container.firstChild;
     await waitFor(() => {
@@ -41,18 +39,15 @@ test('User can change the number of events they want to see', ({ given, when, th
     });
   });
 
-  when('the user chooses to change the number of events displayed,', async () => {
+  when('the user chooses to change the number of events displayed', async () => {
     const button = AppComponent.queryByTestId('numberOfEventsInput');
-
     await userEvent.type(button, '{backspace}{backspace}10');
-  }
-);
+  });
 
-  then('the number of events displayed should update to the selected number.', () => {
+  then('the number of events displayed should update to the selected number', () => {
     const AppDOM = AppComponent.container.firstChild;
     const eventList = within(AppDOM).queryAllByRole('listitem');
     expect(eventList.length).toEqual(10);
-  }
-);
+  });
 });
 });
