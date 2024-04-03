00ca719693e3fa1a84579b5c824c585895f750f2
Caduella <146942933+Caduella@users.noreply.github.com>
2024-04-03 16:28:17 +0200

    Add charts and verification

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import CityEventsChart from './components/CityEventsChart';
-// import EventGenresChart from './components/EventGenresChart';
+import EventGenresChart from './components/EventGenresChart';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
-
 import './App.css';
 
 const App = () => {
@@ -51,8 +50,10 @@ const App = () => {
         setCurrentCity={setCurrentCity}
         setInfoAlert={setInfoAlert} />
       <NumberOfEvents  setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
-      {/* <EventGenresChart events={events} /> */}
-      <CityEventsChart allLocations={allLocations} events={events} />
+      <div className='charts-container'>
+        <EventGenresChart events={events} />
+        <CityEventsChart allLocations={allLocations} events={events}/>
+      </div>
       <EventList events={events} />
     </div>
   );
