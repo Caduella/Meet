3f25e2e2a0e9123d605a68d0e8cedfbdac6c50f6
Caduella <146942933+Caduella@users.noreply.github.com>
2024-04-07 15:41:40 +0200

    useEffect multiple prop values

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -13,7 +13,7 @@ const EventGenresChart = ({ events }) => {
 
     useEffect(() => {
         setData(getData());
-    }, [events]);
+    }, [events, allLocations]);
 
     const getData = () => {
         const data = genres.map(genre => {
